e6ac2e3b07960cc4c555ab93343cece9e92ad12e
Maxime Pley <pleymaxime@gmail.com>
2018-05-31 18:13:02 +0100

    modified code to read the stream

--- twitter-listener.py
+++ twitter-listener.py
@@ -5,23 +5,23 @@ import tweepy
 class StreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        if status.retweeted:
-            return
 
-        description = status.user.description
-        loc = status.user.location
-        text = status.text
-        name = status.user.screen_name
-        user_created = status.user.created_at
-        followers = status.user.followers_count
-        id_str = status.id_str
-        created = status.created_at
-        retweets = status.retweet_count
-        hashtags = status.entities.get('hashtags')
+        if not hasattr(status, 'retweeted_status'):
+            hashtags = status.entities['hashtags']
+            if hashtags != []:
+                text = status.text
+                name = status.user.screen_name
+                user_id = status.user.id
+                created = status.created_at
 
-        f = open("results.txt", "w+")
-        f.write(user_created + name + text + '\n' + hashtags + '\n\n')
-        f.close()
+                # Test that reads the stream - put information in a text file
+                f = open("results.txt", "a")
+                f.write(str(name) + " " + str(user_id) + '\n' + str(created) + '\n' + str(text.encode("utf-8")) + '\n' + str(hashtags) + '\n\n')
+                f.close()
+                return
+
+                # Follow request
+                # API.create_friendship(user_id)
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -29,30 +29,22 @@ class StreamListener(tweepy.StreamListener):
             return False
 
 
+# Read config parameters
 with open('config.yaml', 'r') as f:
     doc = yaml.load(f)
-
-
 hashtags = doc["base"]["TrackTerms"]
-print(hashtags)
 
 # Connection to Twitter API
 try:
     auth = tweepy.OAuthHandler(doc["base"]["API_accessKeyId"], doc["base"]["API_secretAccessKey"])
     auth.set_access_token(doc["base"]["API_tokenKeyId"], doc["base"]["API_secretTokenKey"])
-    api = tweepy.API(auth)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
     print('Connected')
 except:
     print('Not connected')
 
 
-f = open("results.txt", "w+")
-
-for keywords in hashtags:
-    for tweet in tweepy.Cursor(api.search, q=keywords + " -filter:retweets", lang="en").items(5):
-        f.write(str(tweet.created_at) + str(tweet.text.encode("utf-8")) + '\n' + str(tweet.entities.get('hashtags')) + '\n\n')
-f.close()
-
-# stream_listener = StreamListener()
-# stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-# stream.filter(track=settings.TRACK_TERMS)
+# Stream listening
+stream_listener = StreamListener()
+stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+stream.filter(track=hashtags, async=True)
