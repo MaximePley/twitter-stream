f560d3142df45cd9a6d9916c587100a650cf1b2f
Maxime Pley <pleymaxime@gmail.com>
2018-06-04 18:01:02 +0100

    struggle with factory

--- database.py
+++ database.py
@@ -1,9 +1,5 @@
-import redis
-import yaml
 import json
-
-with open('config.yaml', 'r') as f:
-    doc = yaml.load(f)
+import redis
 
 
 class storage(object):
@@ -16,23 +12,22 @@ class storage(object):
         raise AssertionError("Type error: " + type)
 
 
-# Fonctions to store the user data
-# 2 options: Store in Redis or store in text file
 class redisDataStore(storage):
-    def dataStore(self, user):
-        r = redis.StrictRedis(host=doc["datastore"]["database"]["host"], port=doc["datastore"]["database"]["port"], db=0, charset="utf-8", decode_responses=True)
+    def insertUser(self, user, host, port):
+        r = redis.StrictRedis(host=host, port=port, db=0, charset="utf-8", decode_responses=True)
         r.hmset("Data_user", self.user)
 
 
 class textDataStore(storage):
-    def dataStore(self, user):
+    def insertUser(self, user, fileName):
         js = json.dumps(self.user, default=str)
-        f = open(doc["datastore"]["text"]["file_name"], "a")
+        f = open(fileName, "a")
         f.write(js + '\n')
         f.close()
         return
 
-# Only for debugging
-user = {"User id": "user_id", "Created": "created", "Tweet url": "urls"}
-obj = storage.factory("File")
-obj.dataStore(user)
+
+# # Only for debugging
+# user = {"User id": "user_id", "Created": "created", "Tweet url": "urls"}
+# dataStore = storage.factory("File")
+# dataStore.insertUser(user, 'resu.txt')
