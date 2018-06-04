2cd8a2f8797d4b2a83b2192e1bbd60f16cd79536
Maxime Pley <pleymaxime@gmail.com>
2018-06-04 17:59:45 +0100

    Set functions to be called in the main

--- twitterlistener.py
+++ twitterlistener.py
@@ -1,17 +1,6 @@
-import yaml
-import tweepy
 import logging
-import database
+import tweepy
 
-# Set the log config
-logging.basicConfig(filename='execution.log', format='%(asctime)s')
-
-# Read config parameters
-with open('config.yaml', 'r') as f:
-    doc = yaml.load(f)
-hashtags = doc["base"]["TrackTerms"]
-
-# Need to add the storage type argument
 
 class StreamListener(tweepy.StreamListener):
 
@@ -25,34 +14,42 @@ class StreamListener(tweepy.StreamListener):
             return
 
         user_id = status.user.id
-        created = status.created_at
+        created = str(status.created_at)
         urls = status.entities['urls']
         user = {"User id": user_id, "Created": created, "Tweet url": urls}
-        database.textDataStore(user)
+        return user
 
     def on_error(self, status_code):
         if status_code == 420:
-            # returning False in on_data disconnects the stream
-            return False
+            return logging.warning('API rate limit reached')
 
 
 # Follow request
-def follow(user_id):
+def follow(api, user_id):
 
     api.create_friendship(user_id)
 
 
 # Connection to Twitter API
-try:
-    auth = tweepy.OAuthHandler(doc["base"]["API_accessKeyId"], doc["base"]["API_secretAccessKey"])
-    auth.set_access_token(doc["base"]["API_tokenKeyId"], doc["base"]["API_secretTokenKey"])
-    api = tweepy.API(auth, wait_on_rate_limit=True)
-    logging.warning('Connected to the API')
-except:
-    logging.warning('Connection problem')
+def connect(apiAccessKeyId, apiSecretAccessKey, apiTokenKeyId, apiSecretTokenKey):
+
+    try:
+        auth = tweepy.OAuthHandler(apiAccessKeyId, apiSecretAccessKey)
+        auth.set_access_token(apiTokenKeyId, apiSecretTokenKey)
+        api = tweepy.API(auth, wait_on_rate_limit=True)
+        logging.info('Connected to the API')
+        return api
+    except:
+        logging.warning('Connection problem')
 
 
 # Stream listening
-stream_listener = StreamListener()
-stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-stream.filter(track=hashtags, async=True)
+def startStreaming(api, hashtags):
+
+    try:
+        stream_listener = StreamListener()
+        stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+        stream.filter(track=hashtags, async=True)
+        logging.info('Reading the stream')
+    except:
+        logging.warning('Stream broken')
